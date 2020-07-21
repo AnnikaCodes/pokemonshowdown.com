64d8a51526baf5e882b2f9fc1ec1d6f27ec4d265
Annika <hehe@doesntexist.com>
2020-07-21 13:50:09 -0700

    j

--- lib/ntbb-database.lib.php
+++ lib/ntbb-database.lib.php
@@ -24,11 +24,11 @@ class NTBBDatabase {
 
 	function connect() {
 		if (!$this->db) {
-			$connection_string = "host='{$this->server}'";
-			$connection_string .= " username='{$this->username}'";
-			if ($this->password) $connection_string .= " password='{$this->password}'";
-			$connection_string .= " dbname='{$this->database}'";
-			$connection_string .= " port='{$this->port}'";
+			$connection_string = "host={$this->server};";
+			$connection_string .= "username={$this->username}";
+			if ($this->password) $connection_string .= "password={$this->password}";
+			$connection_string .= "dbname={$this->database};";
+			$connection_string .= "port={$this->port}";
 			$this->db = pg_connect($connection_string);
 		}
 	}
